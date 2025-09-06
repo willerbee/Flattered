9e6b473cef85cff80bcbf3af5da8d3fa06109a95
Wilmon Agulo <wilmon.agulo@hapihealthcare.com>
2025-09-06 17:43:22 +0800

    Fix issues for licensing

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,21 @@
+/*
+ * Flattered - A VS Code extension to make any theme flat
+ * Copyright (c) 2025 Wilmon Agulo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
